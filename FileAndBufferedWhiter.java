703de9d5ddaef2d41fd712c226243456eeb45403
Gabriele Rocha <56683790+gabrielerocha22@users.noreply.github.com>
2022-11-07 15:20:20 -0300

    Add files via upload

---
+++ FileAndBufferedWhiter.java
@@ -0,0 +1,25 @@
+package application;
+
+//import java.io.BufferedWhiter;
+////import java.io.FileWhiter;
+//import java.io.IOException;
+//
+//public class FileAndBufferedWhiter {
+//
+//	public static void main(String[] args) {
+//
+//		String[] lines = new String[] { "good morning", "good afternoon", "good night" };
+//
+//		String path = "//Users/gabrielelopesrocha/Desktop";
+//
+//		try (BufferedWhiter bw = new BufferedWhiter(new FileWhiter(path, true))) {
+//			for (String line : lines) {
+//				bw.white(line);
+//				bw.newLine();
+//
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+}
